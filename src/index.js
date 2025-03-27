33b37130371caae080182d38f8166497e5b0a320
AnLaxina <anilovlaxina@gmail.com>
2025-03-27 12:08:01 -0700

    Remove checkList & notes from the ToDo constructor
    For now, I'll prioritize the minimum features rather than adding more things

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
 import ToDoListItem from "./todo"
 
-const todo = new ToDoListItem("cheese", "dog", "duedate now", "idk what a priority", "note", "coolcheck");
+const todo = new ToDoListItem("cheese", "dog", "duedate now", "idk what a priority");
 console.log(todo.description);
--- src/todo.js
+++ src/todo.js
@@ -1,10 +1,8 @@
 export default class ToDo {
-    constructor(title, description, dueDate, priority, notes, checklist) {
+    constructor(title, description, dueDate, priority) {
         this.title = title;
         this.description = description;
         this.dueDate = dueDate;
         this.priority = priority;
-        this.notes = notes;
-        this.checklist = checklist;
     }
 }
