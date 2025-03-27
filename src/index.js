5b50f5fbc0ccb223829ec192bac2587f59632f3d
AnLaxina <anilovlaxina@gmail.com>
2025-03-27 12:28:07 -0700

    Add new method to Project which lists all the available ToDos based on their due date & name

--- src/index.js
+++ src/index.js
@@ -1,4 +1,23 @@
-import ToDoListItem from "./todo"
+import ToDoListItem from "./todo";
+import Project from "./project";
 
-const todo = new ToDoListItem("cheese", "dog", "duedate now", "idk what a priority");
-console.log(todo.description);
+const listOfToDos = [];
+const project = new Project(listOfToDos);
+
+// Date related things, might move these to a separate module
+const date = new Date();
+const day = date.getDate();
+// Turns the month into its actual name instead of just a number
+const month = date.toLocaleString("default", { "month": "long" });
+const year = date.getFullYear();
+
+const actualDate = `${month} ${day}, ${year}`;
+
+
+const todo = new ToDoListItem("cheese", "dog", actualDate, "idk what a priority");
+const todo2 = new ToDoListItem("bacon", "dog", actualDate, "idk what a priority");
+
+listOfToDos.push(todo);
+listOfToDos.push(todo2);
+
+project.viewAllTodos();
--- src/project.js
+++ src/project.js
@@ -2,4 +2,12 @@ export default class Project {
     constructor(listOfToDos) {
         this.listOfToDos = listOfToDos;
     }
+
+    viewAllTodos() {
+        console.log("List of All To-Do Items:");
+        for (const toDo of this.listOfToDos) {
+            // For now let's just print the title and duedate to the console
+            console.log(`${toDo.title} - Due Date: ${toDo.dueDate}`)
+        }
+    }
 }
