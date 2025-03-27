88b7ce20335b4b0636cc48d39ccb44f33fc3ed5d
AnLaxina <anilovlaxina@gmail.com>
2025-03-27 12:34:37 -0700

    Create datetime class to simplify formatting dates

--- src/index.js
+++ src/index.js
@@ -1,21 +1,12 @@
 import ToDoListItem from "./todo";
 import Project from "./project";
+import DateTime from "./datetime";
 
 const listOfToDos = [];
 const project = new Project(listOfToDos);
 
-// Date related things, might move these to a separate module
-const date = new Date();
-const day = date.getDate();
-// Turns the month into its actual name instead of just a number
-const month = date.toLocaleString("default", { "month": "long" });
-const year = date.getFullYear();
-
-const actualDate = `${month} ${day}, ${year}`;
-
-
-const todo = new ToDoListItem("cheese", "dog", actualDate, "idk what a priority");
-const todo2 = new ToDoListItem("bacon", "dog", actualDate, "idk what a priority");
+const todo = new ToDoListItem("cheese", "dog", DateTime.getCurrentDate(), "idk what a priority");
+const todo2 = new ToDoListItem("bacon", "dog", DateTime.getCurrentDate(), "idk what a priority");
 
 listOfToDos.push(todo);
 listOfToDos.push(todo2);
